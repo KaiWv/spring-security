c23eb912d439f51810a76a298247b52cdebeeee0
wukai <1361446189@qq.com>
2022-05-30 17:28:35 +0800

    init

---
+++ src/test/java/com/kaiw/springserurity/SpringSerurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiw.springserurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSerurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
